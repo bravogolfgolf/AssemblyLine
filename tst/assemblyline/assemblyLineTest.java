1ed252c77372249c737de0145a6ebe9d34b37939
Brian Gibson <bggibson@hotmail.com>
2017-01-29 00:38:46 +0100

    Initial commit

---
+++ tst/assemblyline/assemblyLineTest.java
@@ -0,0 +1,15 @@
+package assemblyline;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+
+public class assemblyLineTest {
+
+    @Test
+    public void test() {
+
+        assertFalse(true);
+    }
+
+}
